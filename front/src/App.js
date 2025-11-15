4de769c4ef147140feebbfac80010eb663eee781
mukito2117 <mukito2117@gmail.com>
2025-11-16 03:08:32 +0530

    commit

--- front/src/App.js
+++ front/src/App.js
@@ -22,7 +22,7 @@ useEffect(() => {
 
   fetchData(); // Initial fetch
 
-  const intervalId = setInterval(fetchData, 5000); // Fetch every 5 seconds
+  const intervalId = setInterval(fetchData, 1000); // Fetch every 5 seconds
 
   return () => clearInterval(intervalId); // Cleanup on unmount
 }, []);
