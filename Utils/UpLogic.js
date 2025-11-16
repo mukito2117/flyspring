fe90038b17bddf423b28bd67f1db35383764f0f4
mukito2117 <mukito2117@gmail.com>
2025-11-16 08:20:40 +0530

    commit

--- Utils/UpLogic.js
+++ Utils/UpLogic.js
@@ -264,7 +264,6 @@ function formatTimestampIST() {
   return `${dd}-${mmm}-${yyyy} ${hh}:${mm}:${ss}`;
 }
 
-
 function addLog(strike, entry, calldiff, putdiff) {
   const timestamp = formatTimestamp();
   const logEntry = {
